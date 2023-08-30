f3876211ba2372071dacb6cf0d3391a74fd1b15c
studyworld123 <133588905+studyworld123@users.noreply.github.com>
2023-08-30 15:29:36 +0530

    Add files via upload

---
+++ bookdetails.sql
@@ -0,0 +1,12 @@
+use booksdb;
+create table Books(book_title varchar(60),Author varchar(25),genre varchar(20),publication_year int(20),price int(20));
+insert into books values("Lets C","Yashavant kanetkar","C language",2017,369);
+insert into books values("network programming ","lewis van winkle","network programming",2019,2888);
+insert into books values("python programming","reema thareja","python",2023,593);
+insert into books values("clean code","Robert C. Martin","Agile",2008,500);
+insert into books values("code complete","Steve McConnell","microsoft press",1993,600);
+select * from books;
+update books set price=550 where price=369;
+select * from books;
+delete from books where book_title="Lets C";
+select * from books;
